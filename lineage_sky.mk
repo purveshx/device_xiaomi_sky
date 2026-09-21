57f8dd5fc517491904897ee1bdd3f8ca6b14273b
purveshx <purveshbhangale1425@gmail.com>
2026-09-21 09:30:33 +0530

    Update lineage_sky.mk

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -15,13 +15,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit common Bliss configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/lineage/config/BoardConfigReservedSize.mk)
-
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
+
+# Axion Flags
+USE_RESERVED_SIZE := true
+AXION_CAMERA_REAR_INFO := 50,2
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := purvesh
+AXION_PROCESSOR := Snapdragon™_4_Gen_2
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier
 PRODUCT_DEVICE := sky
